e4b1a4f8abf9b810e50b5410d07183f94f5340e4
ArijitARZ <143308314+ArijitARZ@users.noreply.github.com>
2023-12-02 12:27:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener('click', function () {
+    
+   
+    
+});
+alert("Happy Birthday Kaushik🍰🎂");
+alert("Increase Your Mobile Volume For Listening Song.🎵🎧🔈");
+
+function next() {
+
+  
+  document.querySelector(".cont").style.display= "none";
+ 
+   document.querySelector(".second_page").style.display = "block";
+
+document.querySelector(".b2").style.display = "none";
+
+
+document.querySelector(".b1").style.display = "block"; document.querySelector("body").style.overflow = "scroll";
+
+ }
+function prev() {
+
+  
+  document.querySelector(".cont").style.display= "block";
+ 
+   document.querySelector(".second_page").style.display = "none";
+   
+document.querySelector(".b2").style.display = "block";
+
+
+document.querySelector(".b1").style.display = "none";
+   document.querySelector("body").style.overflow = "hidden";
+
+}
